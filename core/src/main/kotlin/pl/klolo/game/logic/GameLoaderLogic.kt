111e572eba7f74d163f54e906b0b592597a52b80
klolo <kamil.lolo@mail.com>
2018-09-13 21:56:35 +0200

    externalization of music level, visual changes in loader

--- core/src/main/kotlin/pl/klolo/game/logic/GameLoaderLogic.kt
+++ core/src/main/kotlin/pl/klolo/game/logic/GameLoaderLogic.kt
@@ -1,6 +1,5 @@
 package pl.klolo.game.logic
 
-import box2dLight.Light
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.audio.Music
 import com.badlogic.gdx.audio.Sound
@@ -10,28 +9,22 @@ import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.TextureAtlas
 import com.badlogic.gdx.scenes.scene2d.actions.Actions.delay
-import pl.klolo.game.configuration.Colors
+import pl.klolo.game.common.addSequence
+import pl.klolo.game.common.execute
 import pl.klolo.game.engine.*
-import pl.klolo.game.entity.EntityRegistry
-import pl.klolo.game.entity.SpriteWithCustomRendering
-import pl.klolo.game.entity.TextEntity
-import pl.klolo.game.entity.createEntity
+import pl.klolo.game.entity.*
 import pl.klolo.game.event.EventProcessor
 import pl.klolo.game.event.OpenMainMenu
 import pl.klolo.game.event.RegisterEntity
-import pl.klolo.game.common.addSequence
-import pl.klolo.game.common.execute
-import pl.klolo.game.entity.EntityLogicWithRendering
 
 class GameLoaderLogic(
-        private val gameLighting: GameLighting,
         private val eventProcessor: EventProcessor,
         private val entityRegistry: EntityRegistry) : EntityLogicWithRendering<SpriteWithCustomRendering> {
 
     private val textConfiguration = entityRegistry.getConfigurationById("text")
     private lateinit var progressBar: Sprite
     private lateinit var progressBarFill: Sprite
-    private lateinit var pulsingLightAnimation: PulsingLightAnimation
+    private lateinit var background: Sprite
     private lateinit var infoLabel: TextEntity
 
     private var centerX = 0f
@@ -39,7 +32,6 @@ class GameLoaderLogic(
     private var progress = 0f
 
     private val loaderDelay = .1f
-    private lateinit var mainLight: Light
 
     override val initialize: SpriteWithCustomRendering.() -> Unit = {
         Gdx.app.debug(this.javaClass.name, "initialize")
@@ -113,21 +105,13 @@ class GameLoaderLogic(
         centerX = Gdx.graphics.width.toFloat() / 2
         centerY = Gdx.graphics.height.toFloat() / 2
 
-        val logoLight = gameLighting.createPointLight(200, Colors.blueLight, 300f, centerX, centerY)
-        mainLight = gameLighting.createPointLight(200, Colors.ambient, 500f, centerX, centerY)
-
-        pulsingLightAnimation = PulsingLightAnimation(logoLight)
-                .apply {
-                    delta = 1f
-                    minDistance = 150
-                    distanceGrow = 250
-                }
-
         progressBar = Sprite(Texture(Gdx.files.internal("assets/lifebar-background.png")))
         progressBarFill = Sprite(Texture(Gdx.files.internal("assets/white.png")))
+        background = Sprite(Texture(Gdx.files.internal("assets/dark.png")))
     }
 
     override val draw: SpriteWithCustomRendering.(batch: Batch, camera: OrthographicCamera) -> Unit = { batch, _ ->
+        batch.draw(background, 0f, 0f, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
         batch.draw(progressBarFill, centerX - infoLabel.getFontWidth() / 2, centerY, infoLabel.getFontWidth(), 10f)
         batch.draw(progressBar, centerX - infoLabel.getFontWidth() / 2, centerY, infoLabel.getFontWidth() * progress, 10f)
     }
@@ -137,7 +121,6 @@ class GameLoaderLogic(
     }
 
     override val onUpdate: SpriteWithCustomRendering.(Float) -> Unit = {
-        pulsingLightAnimation.update()
     }
 
     private fun createInfoLabel(): TextEntity {
@@ -154,6 +137,7 @@ class GameLoaderLogic(
                 .apply {
                     x = Gdx.graphics.width.toFloat() / 2 - getFontWidth() / 2
                     y = Gdx.graphics.height.toFloat() / 2 - (getFontHeight() * 2)
+                    color.a = 0.8f
                 }
     }
 }
